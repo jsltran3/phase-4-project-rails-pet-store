9dc5f4219a0fe80fa6b94026897f18a2ae097b17
Joseph <jsltran@gmail.com>
2023-04-26 10:36:00 -0700

    migrating to tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,27 @@
-import React, { useContext, useState, useEffect} from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 // import './App.css';
-import NavBar from './components/NavBar'
-import Login from './components/Login'
-import About from './components/About'
-import PetStore from './components/pet_daycare/PetStore'
-import PetItem from './components/pet/PetItem'
-import Location from './components/location/Location'
-import PetStoreOverview from './components/pet_daycare/PetStoreOverview'
+import NavBar from "./components/NavBar";
+import Login from "./components/Login";
+import About from "./components/About";
+import PetStore from "./components/pet_daycare/PetStore";
+import PetItem from "./components/pet/PetItem";
+import Location from "./components/location/Location";
+import PetStoreOverview from "./components/pet_daycare/PetStoreOverview";
 // import Geocode from "react-geocode";
-import { VisibileUserName } from './components/context/VisibleUsername'
-import styled from "styled-components";
-
-
+import { VisibileUserName } from "./components/context/VisibleUsername";
 
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
-      <Route path="/about"
-          element={ <About/> }
-      />
-      <Route
-        path="/petstores"
-          element={<PetStore/>}
-      />
-      <Route
-        path="/petitems"
-          element={<PetItem/>}
-      />
+        <Route path="/about" element={<About />} />
+        <Route path="/petstores" element={<PetStore />} />
+        <Route path="/petitems" element={<PetItem />} />
       </Routes>
-      </>
+    </>
   );
- 
 }
 
 export default App;
