ea3fa6a50d5ae0c568dcbe9b8265677f283fe756
Joseph <jsltran@gmail.com>
2023-05-30 01:53:07 -0700

    add tables

--- db/migrate/20230530044727_create_items.rb
+++ db/migrate/20230530044727_create_items.rb
@@ -2,6 +2,9 @@ class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
       t.string :name
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :pet_store, null: false, foreign_key: true
+      t.string :name
 
       t.timestamps
     end
--- db/migrate/20230530044759_create_locations.rb
+++ db/migrate/20230530044759_create_locations.rb
@@ -2,7 +2,8 @@ class CreateLocations < ActiveRecord::Migration[6.1]
   def change
     create_table :locations do |t|
       t.string :name
-
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :pet_store, null: false, foreign_key: true
       t.timestamps
     end
   end
