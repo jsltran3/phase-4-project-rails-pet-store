37d8d008fabfd500db421acb190da5053219902b
Joseph <jsltran@gmail.com>
2023-05-30 02:47:57 -0700

    add pet store create+update method

--- app/controllers/pet_stores_controller.rb
+++ app/controllers/pet_stores_controller.rb
@@ -1,2 +1,20 @@
 class PetStoresController < ApplicationController
+    def create 
+        party = Party.create!(party_params)
+        render json: party, status: :created
+    end
+    
+    def update
+        party = Party.find_by(id: params[:id])
+        user_id = @current_user.id
+
+        if party.users.find_by(id: user_id)
+            party.update(party_params)
+            render json: party
+        else
+            # render json: { errors: [party.errors.full_messages] }, status: :unprocessable_entity
+            render json: { errors: [party.errors.full_messages] }, status: 400
+        end
+    end
+
 end
