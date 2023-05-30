0e020652697eeb0bab63f0cf4925ff2fd63d0a7f
Joseph <jsltran@gmail.com>
2023-05-30 04:12:31 -0700

    add login and login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUpForm";
+import Button from "@mui/material/Button";
+import StorefrontIcon from '@mui/icons-material/Storefront';
 
+function Login({ onLogin }) {
+  const [showLogin, setShowLogin] = useState(true);
 
-function Login() {
   return (
-    <div>
-
-    </div>
+      <Logo>Party Planner</Logo>
+      {showLogin ? (
+        <>
+          <LoginForm onLogin={onLogin} />
+          <Divider />
+          <p className="StyledParagraph">
+            Don't have an account? &nbsp;
+            <Button color="secondary" onClick={() => setShowLogin(false)}>
+              Sign Up
+            </Button>
+          </p>
+        </>
+      ) : (
+        <>
+          <SignUpForm onLogin={onLogin} />
+          <Divider />
+          <p className="StyledParagraph">
+            Already have an account? &nbsp;
+            <Button color="secondary" onClick={() => setShowLogin(true)}>
+              Log In
+            </Button>
+          </p>
+        </>
+      )}
   );
 }
 
