fab38885624d68afc19a2c7bf18e6e276db9abee
Joseph <jsltran@gmail.com>
2023-05-30 02:39:00 -0700

    add items controller update method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,37 +15,4 @@ class ItemsController < ApplicationController
         end
     end
 
-    def index 
-        items = @current_user.items.all
-
-        if session[:user_id]
-            render json: items
-        else
-            render json: { errors: ["Not authorized"] }, status: :unauthorized
-        end
-    end
-
-    def show
-        item = @current_user.items.find_by(id: params[:id])
-        if item 
-            render json: item
-        else
-            render json: { error: "Item not found" }
-        end
-    end
-
-    def destroy
-        item = @current_user.items.find_by(id: params[:id])
-        if item.user_id == @current_user.id
-            item.destroy
-            head :no_content
-        end
-    end
-
-    private 
-
-    def item_params
-        params.permit(:name, :party_id)
-    end
-
 end
