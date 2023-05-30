e1d698933bbfc18e109964653600385e349ef22a
Joseph <jsltran@gmail.com>
2023-05-30 02:50:39 -0700

    add show/store/idnex

--- app/controllers/pet_stores_controller.rb
+++ app/controllers/pet_stores_controller.rb
@@ -1,20 +1,51 @@
 class PetStoresController < ApplicationController
     def create 
-        party = Party.create!(party_params)
-        render json: party, status: :created
+        store = PetStore.create!(store_params)
+        render json: store, status: :created
     end
     
     def update
-        party = Party.find_by(id: params[:id])
+        store = PetStore.find_by(id: params[:id])
         user_id = @current_user.id
 
-        if party.users.find_by(id: user_id)
-            party.update(party_params)
-            render json: party
+        if store.users.find_by(id: user_id)
+            store.update(store_params)
+            render json: store
         else
-            # render json: { errors: [party.errors.full_messages] }, status: :unprocessable_entity
-            render json: { errors: [party.errors.full_messages] }, status: 400
+            # render json: { errors: [PetStore.errors.full_messages] }, status: :unprocessable_entity
+            render json: { errors: [store.errors.full_messages] }, status: 400
         end
     end
+
+    def index 
+        stores = PetStore.all
+
+        if session[:user_id]
+            render json: stores
+        else
+            render json: { errors: ["Not authorized"] }, status: :unauthorized
+        end
+    end
+
+    def show
+        store = PetStore.find_by(id: params[:id])
+        if store 
+            render json: store
+        else
+            render json: { error: "Store not found" }
+        end
+    end
+
+    def destroy 
+        store = PetStore.find_by(id: params[:id])
+        user_id = @current_user.id
+        if store.users.find_by(id: user_id)
+            store.destroy
+            head :no_content
+        else
+            render json: { error: "Bad request, cannot be deleted" }, status: 400
+        end
+    end
+
 
 end
