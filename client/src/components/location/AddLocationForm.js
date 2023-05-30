38f8c3d6e9bbf309cc8098021867be82010141c8
Joseph <jsltran@gmail.com>
2023-05-30 03:56:46 -0700

    add addlocationform component

--- client/src/components/location/AddLocationForm.js
+++ client/src/components/location/AddLocationForm.js
@@ -1,10 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import ChooseStoreDropdown from "../store/ChoosePetStoreDropDown";
+import swal from "sweetalert";
 
+function AddLocationForm({
+  stores,
+  onChooseStore,
+  onAddLocation,
+  chosenStore,
+}) {
+  console.log("stores within AddLocationForm child component: ", stores);
 
-function AddLocationForm() {
+  const [createLocationFormData, setCreateLocationFormData] = useState({
+    name: "",
+  });
+
+  const handleCreateLocationChange = (e) => {
+    setCreateLocationFormData({
+      ...createLocationFormData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleCreate = (e) => {
+    e.preventDefault();
+    const id = chosenStore.id;
+    console.log(
+      "id in handleCreate function in AddLocationForm child component: ",
+      id
+    );
+    fetch(`/stores/${id}/location`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: createLocationFormData["location_name"],
+        Store_id: id,
+      }),
+    })
+      .then((response) => response.json())
+      .then((newLocation) => {
+        onAddLocation(newLocation);
+        swal("Location added!");
+      });
+  };
+
   return (
     <div>
-
+      <ChooseStoreDropdown stores={stores} onChooseStore={onChooseStore} />
+      <h2>Add Location</h2>
+      <form>
+        <label htmlFor="name">Name of Location:</label>
+        <br />
+        <input
+          onChange={handleCreateLocationChange}
+          type="text"
+          id="name"
+          name="location_name"
+        />
+        <br />
+        <input onClick={handleCreate} type="submit" />
+      </form>
     </div>
   );
 }
