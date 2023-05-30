85753376cd846218f618d6626bece062c638516b
Joseph <jsltran@gmail.com>
2023-05-30 02:05:34 -0700

    models

--- app/models/pet_store.rb
+++ app/models/pet_store.rb
@@ -1,2 +1,10 @@
 class PetStore < ApplicationRecord
+    has_many :items, dependent: :destroy
+    has_many :users, through: :items
+
+    has_one :location, dependent: :destroy
+    has_many :users, through: :location
+
+    validates :name, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    has_many :items
+    has_many :pet_stores, through: :items
+    
+    has_secure_password
+  
+    has_one :location
+    has_many :pet_stores, through: :location
+  
+    validates :username, presence: true, uniqueness: true
 end
